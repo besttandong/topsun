eaca2b4ce8b80974f863882a7824482cda5564df
Administrator <Administrator@Carrod-PC>
2013-06-27 13:45:47 +0800

    d

--- com.topsun.poclient.application/src/com/topsun/posclient/application/common/CallPOSServer.java
+++ com.topsun.poclient.application/src/com/topsun/posclient/application/common/CallPOSServer.java
@@ -1,10 +1,5 @@
 package com.topsun.posclient.application.common;
 
-import javax.ws.rs.core.MediaType;
-
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
 
 /**
  * 
@@ -24,19 +19,19 @@ public class CallPOSServer {
 	 * @param args
 	 */
 	public void call(String path, Object args){
-		Client c = Client.create();
-		WebResource r = c.resource(URL+path);
-		ClientResponse response = r.type(MediaType.APPLICATION_XML).put(ClientResponse.class, args);
-		
-		System.out.println( response.getHeaders().get("Content-Type"));
-		System.out.println(response.getStatus() );
-		String entity = response.getEntity(String.class);
-		System.out.println(entity);
-		
-		
-		ClientResponse sss = r.type(MediaType.APPLICATION_XML).put(ClientResponse.class, args); 
-		
-//		ClientResponse response = r.path(URL+path).accept(MediaType.APPLICATION_XML)
-//		   .put(ClientResponse.class, c);
+//		Client c = Client.create();
+//		WebResource r = c.resource(URL+path);
+//		ClientResponse response = r.type(MediaType.APPLICATION_XML).put(ClientResponse.class, args);
+//		
+//		System.out.println( response.getHeaders().get("Content-Type"));
+//		System.out.println(response.getStatus() );
+//		String entity = response.getEntity(String.class);
+//		System.out.println(entity);
+//		
+//		
+//		ClientResponse sss = r.type(MediaType.APPLICATION_XML).put(ClientResponse.class, args); 
+//		
+////		ClientResponse response = r.path(URL+path).accept(MediaType.APPLICATION_XML)
+////		   .put(ClientResponse.class, c);
 	}
 }
