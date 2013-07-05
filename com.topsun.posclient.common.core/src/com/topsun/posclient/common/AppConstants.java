8a8b15004a8004cc572f480bcde21863bf90ea8f
besttandong <besttandong@gmail.com>
2013-07-04 21:35:07 -0700

    修改数据文件路径

--- com.topsun.posclient.common.core/src/com/topsun/posclient/common/AppConstants.java
+++ com.topsun.posclient.common.core/src/com/topsun/posclient/common/AppConstants.java
@@ -8,39 +8,30 @@ package com.topsun.posclient.common;
 public class AppConstants {
 	
 	public static String LOGIN_USER = "loginUser";
-
 	public static String APPLICATION_ID = "posclient";
 	
 	public static String DATA_USER_PATH = "data\\download\\user_data\\";
-	
 	public static String DATA_SETTLEACCWAY_PATH = "data\\download\\settleaccway_data\\";
-	
-	public static String DATA_USER_FILENAME = "data\\download\\user_data\\data_user.xml";
-	
+	public static String DATA_USER_FILENAME = "data\\download\\data_user.xml";
 	public static String DATA_REPOSITORY_PATH = "data\\download\\repository_data\\";
-	
 	public static String DATA_CASHIERMODE_FILENAME = "data\\download\\data_cashiermode.xml";
-	
 	public static String DATA_SHOP_FILENAME = "data\\download\\data_shop.xml";
-	
 	public static String DATA_ITEM_FILENAME = "data\\download\\data_item.xml";
-	
-	static String DATA_COMMON_PATH = "data\\download\\";
-	
+	public static String DATA_COMMON_PATH = "data\\download\\";
+	public static String DATA_COMMON_PATH_BACK = "data\\download\\back\\";
 	
 	//===================================================================================
 	
-	public static String DATA_STOREPAY_PATH = "data\\upload\\storepay_data\\";
-	
+	public static String DATA_SHOPPAY_PATH = "data\\upload\\shoppay_data\\";
+	public static String DATA_SHOPPAY_PATH_BACK = "data\\upload\\shoppay_data\\back\\";
 	public static String DATA_PARTSALES_PATH = "data\\upload\\partsales_data\\";
-	
-	public static String DATA_ADJUSTSTORE_PATH = "data\\upload\\adjustshop_data\\";
-	
+	public static String DATA_ADJUSTSHOP_PATH = "data\\upload\\adjustshop_data\\";
+	public static String DATA_ADJUSTSHOP_PATH_BACK = "data\\upload\\adjustshop_data\\back\\";
 	public static String DATA_ADJUSTREPOSITORY_PATH = "data\\upload\\adjustrepository_data\\";
-	
-	
+	public static String DATA_ADJUSTREPOSITORY_PATH_BACK = "data\\upload\\adjustrepository_data\\back\\";
 	
 	public static String DATA_PAYMENT_PATH = "data\\upload\\payment_data\\";
-	
+	public static String DATA_PAYMENT_PATH_BACK = "data\\upload\\payment_data\\back\\";
 	public static String DATA_RECEIVE_PATH = "data\\upload\\receive_data\\";
+	public static String DATA_RECEIVE_PATH_BACK = "data\\upload\\receive_data\\back\\";
 }
